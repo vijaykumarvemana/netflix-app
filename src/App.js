e6c02314e09159b2f44b1f4f98cd8278767e39ab
Sai Krishna Goka <gskr1803@gmail.com>
2021-08-27 15:41:18 +0100

    Krishna-implementedMainPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Mainpage from './components/Mainpage';
 import './App.css';
+import Main2 from './components/Main2';
 
 function App() {
   return (
     <>
   <Mainpage/>
+  
     </>
   );
 }
--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,30 +1,36 @@
 import {Component} from 'react'
-import { Container, Row, Col, Card} from 'react-bootstrap'
+import { Container, Row, Col, Card, Form, Button} from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
 
 class Mainpage extends Component {
 
     state = {
-        data : []
-        
+        data : [],
+        movies :['titanic', 'harry%20potter', 'batman']
     }
 
-    componentDidMount = async() => {
+    componentDidMount = () => {
         try {
            
-            let response = await fetch('http://www.omdbapi.com/?apikey=829a56e3&s=titanic',{
+            let responseofpromises= this.state.movies.map(categ => fetch('http://www.omdbapi.com/?apikey=829a56e3&s='+categ,{
                 method: 'GET'
             })
-            //console.log(response)
-            let responsejson = await response.json()
-            //console.log(responsejson.Search)
-                this.setState({
-                   data : responsejson.Search
+            .then(responsepromises => responsepromises.json())
+              )
+             Promise.all(responseofpromises).then(arrayofresponsesdata => {
+              
+                   console.log(arrayofresponsesdata)
+                let work = arrayofresponsesdata.map(ele => {
+                    return ele.Search
                 })
+                console.log("hey Iam Work ", work)
+                this.setState({
+                    data : work
+                })
+                console.log("hey Iam state updated", this.state.data[0])
+              })
            
-            console.log("Hey Iam arrays",this.state.data)
-            }
+        }
              catch (error) {
             console.log(error)
         }
@@ -35,20 +41,51 @@ class Mainpage extends Component {
         return(
             <>
                 <Container className='mt-5'>
-                    <Row>
-                       { this.state.data.map(obj => 
-                       <Col sm={6} md={3} lg={3} className='px-1 no-gutters mb-2'>
+                <Form>
+                    <Form.Group className="mb-3" >
+                    <Form.Control type="text" placeholder="search ...." />
+                        </Form.Group>
+                    
+                </Form>
+                    <Row><h3>{this.state.movies[0]}</h3></Row>
+                    <Row className='mt-2'>
+                        
+                    {this.state.data.length > 0 && this.state.data[0].map(ele => {
+                       return <Col sm={6} md={3} lg={3} className='px-1 no-gutters mb-2'>
                         <Card >
-                            <Card.Img variant="top" src={obj.Poster} className='img-fluid' style={{ height: '25rem'}}/>
+                            <Card.Img variant="top" src={ele.Poster} className='img-fluid' style={{ height: '25rem'}}/>
                            
                             </Card>
                         </Col>
                         
-                       )
+                        })
                     }
-                      
-                       
-                    
+                    </Row>
+                    <Row><h3>{this.state.movies[1]}</h3></Row>
+                    <Row className='mt-2'>
+                    {this.state.data.length > 0 && this.state.data[1].map(ele => {
+                       return <Col sm={6} md={3} lg={3} className='px-1 no-gutters mb-2'>
+                        <Card >
+                            <Card.Img variant="top" src={ele.Poster} className='img-fluid' style={{ height: '25rem'}}/>
+                           
+                            </Card>
+                        </Col>
+                        
+                        })
+                    }
+                    </Row>
+                    <Row><h3>{this.state.movies[2]}</h3></Row>
+                    <Row className='mt-2'>
+                    {this.state.data.length > 0 && this.state.data[2].map(ele => {
+                       return <Col sm={6} md={3} lg={3} className='px-1 no-gutters mb-2'>
+                        <Card >
+                            <Card.Img variant="top" src={ele.Poster} className='img-fluid' style={{ height: '25rem'}}/>
+                           
+                            </Card>
+                        </Col>
+                        
+                        })
+                    }
                     </Row>
                 </Container>
 
