32ffec16f5efe4c83c4a3d814b755533e97b28ce
vijay kumar vemana <vijaykumarvemana18@gmail.com>
2021-09-24 14:50:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,12 +13,21 @@ import MainpageB from './components/MainpageB';
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <NavBar />
 
       <MainpageB title="Star Wars"  />
 
     
      <Footer />
+=======
+<NavBar />
+      <MainpageB />
+
+   
+  
+    <Footer />
+>>>>>>> Stashed changes
     
 
     </div>
